1f24ef5b8f0c91a006b3b00a0a59765b9bca268b
Brajesh Singh <brajesh@cosmiccoders.com>
2017-05-23 09:32:49 +0530

    reformat the code

--- bp-community-activity-on-profile.php
+++ bp-community-activity-on-profile.php
@@ -13,75 +13,91 @@
  * 
 */
 
-if(!defined("BPCOM_ACTIVITY_SLUG"))
-    define ("BPCOM_ACTIVITY_SLUG", "all-activity");
+if ( ! defined( "BPCOM_ACTIVITY_SLUG" ) ) {
+	define( "BPCOM_ACTIVITY_SLUG", "all-activity" );
+}
 /*localization*/
 function bp_com_activity_load_textdomain() {
-        $locale = apply_filters( 'bp_com_activity_load_textdomain_get_locale', get_locale() );
-        
-      
+	$locale = apply_filters( 'bp_com_activity_load_textdomain_get_locale', get_locale() );
+
+
 	// if load .mo file
-	if ( !empty( $locale ) ) {
-		$mofile_default = sprintf( '%slanguages/%s.mo', plugin_dir_path(__FILE__), $locale );
-              
+	if ( ! empty( $locale ) ) {
+		$mofile_default = sprintf( '%slanguages/%s.mo', plugin_dir_path( __FILE__ ), $locale );
+
 		$mofile = apply_filters( 'bp_com_activity_load_textdomain_mofile', $mofile_default );
-		
-                if ( file_exists( $mofile ) ) {
-                    // make sure file exists, and load it
+
+		if ( file_exists( $mofile ) ) {
+			// make sure file exists, and load it
 			load_textdomain( "bpcomac", $mofile );
 		}
 	}
 }
-add_action ( 'bp_loaded', 'bp_com_activity_load_textdomain', 2 );
+
+add_action( 'bp_loaded', 'bp_com_activity_load_textdomain', 2 );
 
 //add all activity to nav
-function bp_add_community_activity_to_profile_nav(){
-    global $bp;
-   if(!is_user_logged_in())
-       return;
-   
-   $activity_link = bp_core_get_user_domain(bp_loggedin_user_id()) . bp_get_activity_slug() . '/';
-   //add to user activity subnav if it is logged in users profile
-   bp_core_new_subnav_item( array( 'name' => __( 'All Activity', 'bpcomac' ), 'slug' => BPCOM_ACTIVITY_SLUG, 'parent_url' => $activity_link, 'parent_slug' => $bp->activity->slug, 'screen_function' => 'bp_community_activity_screen', 'position' => 5,'user_has_access'=>  bp_is_my_profile() ) );
+function bp_add_community_activity_to_profile_nav() {
+	global $bp;
+	if ( ! is_user_logged_in() ) {
+		return;
+	}
 
-	
+	$activity_link = bp_core_get_user_domain( bp_loggedin_user_id() ) . bp_get_activity_slug() . '/';
+	//add to user activity subnav if it is logged in users profile
+	bp_core_new_subnav_item( array( 'name'            => __( 'All Activity', 'bpcomac' ),
+	                                'slug'            => BPCOM_ACTIVITY_SLUG,
+	                                'parent_url'      => $activity_link,
+	                                'parent_slug'     => $bp->activity->slug,
+	                                'screen_function' => 'bp_community_activity_screen',
+	                                'position'        => 5,
+	                                'user_has_access' => bp_is_my_profile()
+	) );
+
+
 }
-add_action( 'bp_activity_setup_nav','bp_add_community_activity_to_profile_nav' );
+
+add_action( 'bp_activity_setup_nav', 'bp_add_community_activity_to_profile_nav' );
 //just load the home page
-function bp_community_activity_screen(){
-    
-        do_action( 'bp_community_activity_screen' );
-	bp_core_load_template( apply_filters( 'bp_activity_template_community_activity', 'members/single/home' ) ); 
+function bp_community_activity_screen() {
+
+	do_action( 'bp_community_activity_screen' );
+	bp_core_load_template( apply_filters( 'bp_activity_template_community_activity', 'members/single/home' ) );
 }
+
 //filter ajax request
 
 
 //load te,mplate
- //do the magic here by filtering
-function bp_community_ajax_filter($query_string, $object ){
-    global $bp;
-    //if user is logged in & current action is community on profile tab
-    if(bp_is_my_profile()&&  bp_is_activity_component()&&$bp->current_action==BPCOM_ACTIVITY_SLUG){
-        $comm_query= "user_id=0&scope=0";//just make it so it prints directory :)
-        $query_string= $query_string?$query_string."&".$comm_query:$comm_query;
+//do the magic here by filtering
+function bp_community_ajax_filter( $query_string, $object ) {
+	global $bp;
+	//if user is logged in & current action is community on profile tab
+	if ( bp_is_my_profile() && bp_is_activity_component() && $bp->current_action == BPCOM_ACTIVITY_SLUG ) {
+		$comm_query   = "user_id=0&scope=0";//just make it so it prints directory :)
+		$query_string = $query_string ? $query_string . "&" . $comm_query : $comm_query;
 
-    }
-        
-    return $query_string;
+	}
+
+	return $query_string;
 }
 
-add_filter( 'bp_ajax_querystring', "bp_community_ajax_filter",12,2);
+add_filter( 'bp_ajax_querystring', "bp_community_ajax_filter", 12, 2 );
 
 //fix delete link on profile activity
-add_filter("bp_activity_delete_link","bpcom_fix_delete_link",10,2);
-function bpcom_fix_delete_link($del_link,$activity){
-    global $bp;
-       if(bp_is_my_profile()&&  bp_is_activity_component()&&$bp->current_action==BPCOM_ACTIVITY_SLUG){
-           //let us apply our mod
-           if ( ( is_user_logged_in() && $activity->user_id == $bp->loggedin_user->id ) || $bp->loggedin_user->is_super_admin )
-		return $del_link;
-           return '';
-       }
-    return $del_link;
+add_filter( "bp_activity_delete_link", "bpcom_fix_delete_link", 10, 2 );
+function bpcom_fix_delete_link( $del_link, $activity ) {
+	global $bp;
+	if ( bp_is_my_profile() && bp_is_activity_component() && $bp->current_action == BPCOM_ACTIVITY_SLUG ) {
+		//let us apply our mod
+		if ( ( is_user_logged_in() && $activity->user_id == $bp->loggedin_user->id ) || $bp->loggedin_user->is_super_admin ) {
+			return $del_link;
+		}
+
+		return '';
+	}
+
+	return $del_link;
 }
+
 ?>
